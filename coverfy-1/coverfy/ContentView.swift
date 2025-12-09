bc15e57a572e423b7d00a9d8bd06f278303bb44d
camilacirne <camilacirne@hotmail.com>
2025-12-08 21:24:53 -0300

    start project

---
+++ coverfy-1/coverfy/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  coverfy
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ coverfy-1/coverfy/coverfyApp.swift
@@ -0,0 +1,17 @@
+//
+//  coverfyApp.swift
+//  coverfy
+//
+//  Created by Camila Cirne Pedrosa de Oliveira on 08/12/25.
+//
+
+import SwiftUI
+
+@main
+struct coverfyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
